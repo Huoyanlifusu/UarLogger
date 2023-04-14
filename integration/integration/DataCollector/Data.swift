eac5ea6e6f91ed9bcbabf894a1a9111ce19cc7f7
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-14 22:27:21 +0800

    Add files via upload

--- integration/integration/DataCollector/Data.swift
+++ integration/integration/DataCollector/Data.swift
@@ -4,5 +4,5 @@ import simd
 struct StoredData {
     static var peerPosInARKit: simd_float4?
     static var peerPosInNI: simd_float3?
-    static var camTrans: simd_float4x4?
+    static var distance: Float?
 }
--- integration/integration/DataCollector/DataCollector.swift
+++ integration/integration/DataCollector/DataCollector.swift
@@ -6,33 +6,51 @@ class DataCollector: NSObject {
     
     var logFile: URL? {
         guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
-        let fileName = "ar_data.csv"
+        let fileName = "ar_data2.csv"
         return documentsDirectory.appendingPathComponent(fileName)
     }
     
-    func collectData(_ posA: simd_float4, _ posB: simd_float3, _ frame: ARFrame, _ frameNum: Int) {
+    func collectData(_ posA: simd_float4,
+                     _ posB: simd_float3,
+                     _ frame: ARFrame,
+                     _ distance: Float,
+                     _ frameNum: Int) {
         let data = PosData(peerPosAR: posA.normalize(),
                            peerPosNI: posB,
-                           myPos: frame.camera.transform.columns.3.normalize())
+                           myPos: frame.camera.transform.columns.3.normalize(),
+                           distance: distance)
         pos.append(data)
-        writeTrackedSymptomValues(data.peerPosAR, data.peerPosNI, data.myPos, frameNum)
+        writeTrackedSymptomValues(data.peerPosAR,
+                                  data.peerPosNI,
+                                  data.myPos,
+                                  data.distance,
+                                  frameNum)
     }
     
-    func writeTrackedSymptomValues(_ peerPosAR: simd_float3, _ peerPosNI: simd_float3, _ myPos: simd_float3, _ timestamp: Int) {
+    func writeTrackedSymptomValues(_ peerPosAR: simd_float3,
+                                   _ peerPosNI: simd_float3,
+                                   _ myPos: simd_float3,
+                                   _ distance: Float,
+                                   _ timestamp: Int) {
         let peerPosARStr = "\(peerPosAR.x)+\(peerPosAR.y)+\(peerPosAR.z)"
         let peerPosNIStr = "\(peerPosNI.x)+\(peerPosNI.y)+\(peerPosNI.z)"
         let myPosStr = "\(myPos.x)+\(myPos.y)+\(myPos.z)"
+        let distanceStr = "\(distance)"
         let timeStr = "\(timestamp)"
-        createCSV(peerPosARStr, peerPosNIStr, myPosStr, timeStr)
+        createCSV(peerPosARStr, peerPosNIStr, myPosStr, distanceStr, timeStr)
     }
     
-    func createCSV(_ x: String, _ y: String, _ z: String, _ t: String) {
+    func createCSV(_ x: String,
+                   _ y: String,
+                   _ z: String,
+                   _ d: String,
+                   _ t: String) {
         guard let logFile = logFile else {
             print("没找到本地文件")
             return
         }
 
-        guard let data = "\(x),\(y),\(z),\(t)\n".data(using: String.Encoding.utf8) else { return }
+        guard let data = "\(x),\(y),\(z),\(d),\(t)\n".data(using: String.Encoding.utf8) else { return }
 
         if FileManager.default.fileExists(atPath: logFile.path) {
             if let fileHandle = try? FileHandle(forWritingTo: logFile) {
@@ -43,7 +61,7 @@ class DataCollector: NSObject {
             }
         } else {
             var csvText = "peerPosAR,peerPosNI,MyPos,Time\n"
-            let newLine = "\(x),\(y),\(z),\(t)\n"
+            let newLine = "\(x),\(y),\(z),\(d),\(t)\n"
             csvText.append(newLine)
             do {
                 try csvText.write(to: logFile, atomically: true, encoding: String.Encoding.utf8)
@@ -61,6 +79,7 @@ struct PosData {
     var peerPosAR: simd_float3
     var peerPosNI: simd_float3
     var myPos: simd_float3
+    var distance: Float
 }
 
 extension simd_float4 {
