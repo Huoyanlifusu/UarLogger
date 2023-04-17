c893fda8e4ae171f0f38e659c0bb6cc26870c52e
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-17 17:06:33 +0800

    Add files via upload

---
+++ integration/integration/IMUCollector/CMManager.swift
@@ -0,0 +1,215 @@
+//
+//  CMManager.swift
+//  integration
+//
+//  Created by 张裕阳 on 2023/4/17.
+//
+import Foundation
+import CoreMotion
+import UIKit
+
+class CMManager: CMMotionManager {
+    let viewController: ViewController
+    let imuFreq: Double = 100.0
+    let accegyroQueue = OperationQueue()
+    let motionQueue = OperationQueue()
+    let magnetQueue = OperationQueue()
+    
+    var isRecoding = false
+    var bootTime: TimeInterval
+    
+    var logFile: URL? {
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        return documentsDirectory
+    }
+    
+    var logStringGyro = NSMutableString(string: "")
+    var logStringAcce = NSMutableString(string: "")
+    var logStringMagn = NSMutableString(string: "")
+    var logStringMotion = NSMutableString(string: "")
+    var logStringMotARH = NSMutableString(string: "")
+    var logStringMotMAG = NSMutableString(string: "")
+    
+    init(bootTime: TimeInterval, viewController: ViewController) {
+        self.viewController = viewController
+        self.bootTime = bootTime
+        super.init()
+        self.gyroUpdateInterval = 1.0/imuFreq
+        self.accelerometerUpdateInterval = 1.0/imuFreq
+        self.deviceMotionUpdateInterval = 1.0/imuFreq
+    }
+    
+    func outputRotationData(_ data: CMGyroData?) {
+        if (isRecoding && data != nil) {
+            let msDate = bootTime + data!.timestamp
+            logStringGyro.append(String(format: "%f,%f,%f,%f\r\n",
+                                        msDate,
+                                        data!.rotationRate.x,
+                                        data!.rotationRate.y,
+                                        data!.rotationRate.z))
+        }
+    }
+    
+    func outputAccelerationData(_ data: CMAccelerometerData?) {
+        if (isRecoding && data != nil) {
+            let msDate = bootTime + data!.timestamp
+            logStringAcce.append(String(format: "%f,%f,%f,%f\r\n",
+                                        msDate,
+                                        data!.acceleration.x,
+                                        data!.acceleration.y,
+                                        data!.acceleration.z))
+        }
+    }
+    
+    func outputMagnetometerData(_ data: CMMagnetometerData?) {
+        if (isRecoding && data != nil) {
+            let msDate = bootTime + data!.timestamp
+            logStringMagn.append(String(format: "%f,%f,%f,%f\r\n",
+                                        msDate,
+                                        data!.magneticField.x,
+                                        data!.magneticField.y,
+                                        data!.magneticField.z))
+        }
+    }
+    
+    func outputDeviceMotionData(_ data: CMDeviceMotion?) {
+        if (isRecoding && data != nil) {
+            let msDate = bootTime + data!.timestamp
+            let quat = data!.attitude.quaternion
+            logStringMotion.append(String(format: "%f,%f,%f,%f,%f\r\n",
+                                        msDate,
+                                        quat.w,
+                                        quat.x,
+                                        quat.y,
+                                        quat.z))
+            
+            let rotr = data!.rotationRate
+            let grav = data!.gravity
+            let usracc = data!.userAcceleration
+            logStringMotARH.append(String(format: "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\r\n",
+                                          msDate,
+                                          rotr.x,
+                                          rotr.y,
+                                          rotr.z,
+                                          grav.x,
+                                          grav.y,
+                                          grav.z,
+                                          usracc.x,
+                                          usracc.y,
+                                          usracc.z,
+                                          data!.heading))
+            
+            let calmagfield = data!.magneticField
+            logStringMotMAG.append(String(format: "%f,%f,%f,%f,%d\r\n",
+                                          msDate,
+                                          calmagfield.field.x,
+                                          calmagfield.field.y,
+                                          calmagfield.field.z,
+                                          calmagfield.accuracy.rawValue))
+        }
+    }
+    
+    func startGyroAcceUpdate() {
+        self.startGyroUpdates(to: accegyroQueue, withHandler: {
+            (data: CMGyroData?, error: Error?) in
+            self.outputRotationData(data)
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: "Gyroscope update: \(error)", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.viewController.present(alert, animated: true, completion: nil)
+            }
+        })
+        
+        self.startAccelerometerUpdates(to: accegyroQueue, withHandler: {
+            (data: CMAccelerometerData?, error: Error?) in
+            self.outputAccelerationData(data)
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: "Accelerometer update: \(error)", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.viewController.present(alert, animated: true, completion: nil)
+            }
+        })
+    }
+    
+    func stopGyroAcceUpdate() {
+        self.stopGyroUpdates()
+        self.stopAccelerometerUpdates()
+    }
+    
+    func startMotionUpdate() {
+        self.startDeviceMotionUpdates(using: .xTrueNorthZVertical, to: motionQueue, withHandler: {
+            (data: CMDeviceMotion?, error: Error?) in
+            self.outputDeviceMotionData(data)
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: "DeviceMotion update: \(error)", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.viewController.present(alert, animated: true, completion: nil)
+            }
+        })
+    }
+    
+    func stopMotionUpdate() {
+        self.stopDeviceMotionUpdates()
+    }
+    
+    func startMagnetUpdate() {
+        self.startMagnetometerUpdates(to: magnetQueue, withHandler: {
+            (data: CMMagnetometerData?, error: Error?) in
+            self.outputMagnetometerData(data)
+            if let error = error {
+                let alert = UIAlertController(title: "Error", message: "Magnetometer update: \(error)", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.viewController.present(alert, animated: true, completion: nil)
+            }
+        })
+    }
+    
+    func stopMagnetUpdate() {
+        self.stopMagnetometerUpdates()
+    }
+    
+    func writeStringToFile(_ string: NSMutableString, _ filename: String) {
+        guard let url = logFile else {
+            fatalError()
+        }
+        let filePath = (url.path as NSString)
+                                .appendingPathComponent((filename as NSString)
+                                .appendingPathExtension("txt")!)
+        do {
+            try string.write(toFile: filePath, atomically: true,
+                             encoding: String.Encoding.utf8.rawValue)
+        } catch {
+            print("Error writing string to file: \(error)")
+        }
+    }
+    
+    func startRecording() {
+        isRecoding = true
+        startGyroAcceUpdate()
+        startMotionUpdate()
+        startMagnetUpdate()
+    }
+    
+    func endRecording() {
+        isRecoding = false
+        stopGyroAcceUpdate()
+        stopMotionUpdate()
+        stopMagnetUpdate()
+        writeStringToFile(logStringGyro, "Gyro")
+        writeStringToFile(logStringAcce, "Accel")
+        writeStringToFile(logStringMotion, "Motion")
+        writeStringToFile(logStringMotARH, "MotARH")
+        writeStringToFile(logStringMotMAG, "MotMagnFull")
+        writeStringToFile(logStringMagn, "Magnet")
+        self.logStringMagn = ""
+        self.logStringAcce = ""
+        self.logStringMotARH = ""
+        self.logStringGyro = ""
+        self.logStringMotion = ""
+        self.logStringMotMAG = ""
+    }
+}
